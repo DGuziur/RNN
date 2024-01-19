2c9eefa28ab31706e35e674a4f86e185709c81f5
DGuziur <43893166+DGuziur@users.noreply.github.com>
2024-01-19 16:17:33 +0100

    unit tests

---
+++ website/src/app/components/code-example..component.spec.ts
@@ -0,0 +1,48 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CodeExample } from './code-example.component';
+import { PythonService } from '../services/python.service';
+import { of } from 'rxjs';
+
+describe('CodeExampleComponent', () => {
+  let component: CodeExample;
+  let fixture: ComponentFixture<CodeExample>;
+  let pythonServiceSpy: jasmine.SpyObj<PythonService>;
+
+  beforeEach(() => {
+    pythonServiceSpy = jasmine.createSpyObj('PythonService', {
+      connect: null,
+      runCode: of('mock'),
+      getOutput: of('mock'),
+      disconnect: null,
+    });
+
+    TestBed.configureTestingModule({
+      imports: [CodeExample],
+      providers: [{ provide: PythonService, useValue: pythonServiceSpy }],
+    });
+
+    fixture = TestBed.createComponent(CodeExample);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('terminal should init as false', () => {
+    expect(component.isTerminalOpen).toBeFalsy;
+  });
+
+  it('subscribe should init as null', () => {
+    expect(component.subscribtion).toBeNull;
+  });
+
+  it('runScript works', () => {
+    component.runScript('test');
+    expect(component.isTerminalOpen).toBe(true);
+    expect(component.subscribtion).not.toBeNull;
+  });
+
+  it('stopScript works', () => {
+    component.stopScript;
+    expect(component.isTerminalOpen).toBeFalsy;
+    expect(component.subscribtion).toBeNull;
+  });
+});
